255ea2e5f651d3e1b55e0c322f138c91f418a91d
Jeremy Hammer <jeremyh@cetoncorp.com>
2013-11-29 20:54:10 -0800

    added .screenrc Added push default = simple to gitconfig updated .vim submodule id
    Signed-off-by: Jeremy Hammer <jeremyh@cetoncorp.com>

--- link.sh
+++ link.sh
@@ -6,7 +6,8 @@ mkdir -p ${DOTFILE_PATH}/.vim/autoload
 ln -sf ../vim-pathogen/autoload/pathogen.vim ${DOTFILE_PATH}/.vim/autoload/pathogen.vim
 
 cd
-rm -rf .vim .gitconfig .vimrc .bash_profile .gvimrc .profile .subversion
+rm -rf .vim .gitconfig .vimrc .bash_profile .gvimrc .profile .subversion .screenrc
+ln -s ${DOTFILE_PATH}/.screenrc
 ln -s ${DOTFILE_PATH}/.vim
 ln -s ${DOTFILE_PATH}/.subversion
 ln -s .vim/.vimrc
