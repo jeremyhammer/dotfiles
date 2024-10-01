90efccd4ce3ce3a3299c550e26b3d465446290aa
Jeremy Hammer <jeremy@axiim.com>
2024-10-01 16:07:39 -0700

    update scripts to work on latest ubuntu and update .vim submodule ID
    Signed-off-by: Jeremy Hammer <jeremy@axiim.com>

--- install_deps.sh
+++ install_deps.sh
@@ -3,5 +3,5 @@
 if which yum > /dev/null; then
     sudo yum -y install ctags cscope vim
 elif which apt-get > /dev/null; then
-    sudo apt-get install ctags cscope vim
+    sudo apt-get install exuberant-ctags cscope vim
 fi
